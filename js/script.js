fbb171a9900dc539a5f1da0595447486fed3a57e
Tomoko Kawaguchi <tomo.k.dev@gmail.com>
2022-11-25 02:08:44 +1100

    Added header stylings and js logics

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
+import { createElement, handleActiveClass, clearModal } from "./functions.js";
+import { menuData } from "./data.js";
+
 const appList = document.querySelector(".app-list-wrapper");
 const mainScreen = document.querySelector(".main-screen");
 
-// Toggling app active class
+//======================
+// App List Section
+//======================
+
+// Toggling app list/group active class
 appList.addEventListener("mouseover", () => {
 	mainScreen.classList.add("active-app-list");
 });
@@ -10,33 +17,103 @@ appList.addEventListener("mouseout", () => {
 	mainScreen.classList.remove("active-app-list");
 });
 
-// Toggling util icons list active class
+//======================
+// Util List Section
+//======================
 const screenBody = document.querySelector(".mac-screen__inner");
 const utilItems = document.querySelectorAll(".util-list__item");
 
+// Toggling util icons list active class
 utilItems.forEach((item) => {
 	item.addEventListener("click", (event) => {
 		event.stopPropagation(); // stops the move upwards of bubbling
 		const currentItem = event.target;
 
-		if (currentItem.classList.contains("active-util-list")) {
-			currentItem.classList.remove("active-util-list");
-		} else {
-			const prevItem = document.querySelector(".active-util-list");
-
-			if (prevItem) {
-				prevItem.classList.remove("active-util-list");
-			}
-
-			currentItem.classList.add("active-util-list");
-		}
+		// Add/Remove modal active class
+		handleActiveClass(currentItem, "active-list");
 	});
 });
 
-// To let user click anywhere in the mac screen to close
+const toggleBtns = document.querySelectorAll(".toggle-btn");
+
+toggleBtns.forEach((btn) => {
+	btn.addEventListener("click", (event) => {
+		event.stopPropagation();
+		event.target.closest(".list-modal").classList.toggle("toggleOn");
+	});
+});
+
+//======================
+// Nav List Section
+//======================
+
+const menuItems = document.querySelectorAll(".nav__list__item");
+
+// Toggling nav list active class
+menuItems.forEach((item, index) => {
+	item.addEventListener("click", (event) => {
+		event.stopPropagation(); // stops the move upwards of bubbling
+		const currentItem = event.target;
+		const parentNavIndex = index;
+
+		// If the same button is clicked, exit
+		if (currentItem.classList.contains("active-list")) return;
+
+		// Clear all inside of the previous modal
+		if (document.querySelectorAll(".list-modal").length > 0) {
+			clearModal();
+		}
+
+		// Add/Remove modal active class
+		handleActiveClass(currentItem, "active-list");
+
+		// If list-modal already exists, skip creating new one
+		if (!currentItem.contains(document.querySelector(".active-list .list-modal"))) {
+			// create a new div for modal
+			createElement("div", "", "list-modal", currentItem);
+		}
+
+		// Set up ul for the list of menu items
+		const modal = currentItem.querySelector(".list-modal");
+		createElement("ul", "", "list-modal__list", modal);
+		const navSubmenuUl = modal.querySelector(".list-modal__list");
+
+		// Map data to modal
+		const submenuData = menuData[parentNavIndex].submenuList;
+		submenuData.forEach((submenuInfo) => {
+			const submenuTitle = submenuInfo.submenu;
+			createElement("li", submenuTitle, "list-modal__item", navSubmenuUl);
+
+			// const hasSubmenu = submenuInfo.hasChildren;
+			// const subSumMenu = submenuInfo.subSubMenu; // obj
+			// const li = document.querySelector(".list-modal__item");
+
+			// if (hasSubmenu) {
+			// 	createElement("div", "", "sub-modal", li);
+			// 	const subModal = document.querySelector(".sub-modal");
+			// 	createElement("ul", "", "sub-modal__list", subModal);
+			// 	const subSubArr = Object.entries(subSumMenu);
+
+			// 	subSubArr.forEach((each) => {
+			// 		const secTitle = each[0];
+			// 		const subSubMenusArr = each[1];
+			// 	});
+			// }
+		});
+	});
+});
+
+//======================
+// Global
+//======================
+
+// To let user click anywhere in the mac screen to close by removing the active class
 screenBody.addEventListener("click", () => {
-	const currentActiveUtilItem = document.querySelector(".active-util-list");
+	const currentActiveUtilItem = document.querySelector(".active-list");
 	if (currentActiveUtilItem) {
-		currentActiveUtilItem.classList.remove("active-util-list");
+		// Clear the modal context
+		clearModal();
+		// Remove the active list class
+		currentActiveUtilItem.classList.remove("active-list");
 	}
 });
